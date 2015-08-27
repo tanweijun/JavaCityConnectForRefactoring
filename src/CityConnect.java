369c8f84f2399d559ca09e4fd0c467910b003a6b
tanweijun <a0125360@u.nus.edu>
2015-08-27 18:14:08 +0800

    Rename boolean method 'sameRoute' to 'isSameRoute'

--- src/CityConnect.java
+++ src/CityConnect.java
@@ -225,7 +225,7 @@ public class CityConnect {
 
 			if (existing_start_location == null) { //beginning of empty slots
 				return NOT_FOUND; 
-			} else if (sameRoute(existing_start_location, existing_end_location,
+			} else if (isSameRoute(existing_start_location, existing_end_location,
 					newStartLocation, newEndLocation)) { 
 				return i;
 			}
@@ -293,7 +293,7 @@ public class CityConnect {
 
 			if (existingStartLocation == null) { // empty slot
 				return i;
-			} else if (sameRoute(existingStartLocation, existingEndLocation,
+			} else if (isSameRoute(existingStartLocation, existingEndLocation,
 					newStartLocation, newEndLocation)) {
 				return i;
 			}
@@ -304,7 +304,7 @@ public class CityConnect {
 	/**
 	 * This operation checks if two routes represents the same route.
 	 */
-	private static boolean sameRoute(String startLocation1,
+	private static boolean isSameRoute(String startLocation1,
 			String endLocation1, String startLocation2, String endLocation2) {
 
 		if ((startLocation1 == null) || (endLocation1 == null)
